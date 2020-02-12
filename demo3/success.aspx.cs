89bfa45b06dec4e49f3d441df3ff312499102626
kasula ramesh <ramesh98.kasula@gmail.com>
2020-02-12 18:42:47 +0530

    first commit

---
+++ demo3/success.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+
+public partial class success : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindGridviewData();
+
+        }
+    }
+    public void BindGridviewData()
+    {
+        using (MySqlConnection con = new MySqlConnection("server=localhost;port=3306;database=db;user=root;password=njsinfotech"))
+        {
+            using (MySqlCommand cmd = new MySqlCommand("select*from emplloyees", con))
+            {
+                con.Open();
+                DataSet ds = new DataSet();
+                using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                {
+                    da.Fill(ds);
+                    first.DataSource = ds;
+                    first.DataBind();
+                }
+
+            }
+        }
+    }
+}
+
+    
+    
